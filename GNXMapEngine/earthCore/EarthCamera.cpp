9c337598ef7f1abec297eabd631309e2e60a1a7b
zhouxuguang <zhouxuguang236@126.com>
2024-07-03 14:29:11 +0800

    修复东北天坐标轴向计算的问题

--- GNXMapEngine/earthCore/EarthCamera.cpp
+++ GNXMapEngine/earthCore/EarthCamera.cpp
@@ -12,14 +12,30 @@ EARTH_CORE_NAMESPACE_BEGIN
 EarthCamera::EarthCamera(const Ellipsoid& ellipsoid, const std::string& name) :
     mEllipsoid(ellipsoid),
     Camera(getRenderDevice()->getRenderDeviceType(), name),
-    mEyeGeodetic(degToRad(120), degToRad(23), 6378255),
-    mEyeGeodeticCenter(degToRad(120), degToRad(23), 0)
+    mEyeGeodetic(degToRad(110), degToRad(23), 6398140),
+    mEyeGeodeticCenter(degToRad(110), degToRad(23), 0)
 {
     Vector3d eyePos = ellipsoid.CartographicToCartesian(mEyeGeodetic);
     Vector3d targetPos = ellipsoid.CartographicToCartesian(mEyeGeodeticCenter);
+    
+    // 计算视坐标系的坐标轴的朝向
+    double latitude = radToDeg(mEyeGeodetic.latitude);
+    double longitude = radToDeg(mEyeGeodetic.longitude);
+//    Matrix4x4d viewMatrix = Matrix4x4d::CreateRotation(1, 0, 0, 90 - radToDeg(mEyeGeodetic.latitude)) *
+//                            Matrix4x4d::CreateRotation(0, 0, 1, 90 + radToDeg(mEyeGeodetic.longitude));
+    Matrix4x4d viewMatrix = Matrix4x4d::CreateRotation(0, 0, 1, -90 - radToDeg(mEyeGeodetic.longitude)) *
+                        Matrix4x4d::CreateRotation(1, 0, 0, -90 + radToDeg(mEyeGeodetic.latitude));
+    
+    Vector3f zAxis = Vector3f(viewMatrix[0][1], viewMatrix[1][1], viewMatrix[2][1]);
+    
+    // 计算局部的东北天的坐标轴向
+    Vector3d up = ellipsoid.GeodeticSurfaceNormal(mEyeGeodetic);
+    Vector3d east = Vector3d(-eyePos.y, eyePos.x, 0.0).Normalize();
+    Vector3d north = Vector3d::CrossProduct(up, east);
+    
     LookAt(Vector3f(eyePos.x, eyePos.y, eyePos.z),
            Vector3f(targetPos.x, targetPos.y, targetPos.z),
-           Vector3f::UNIT_Z);
+           Vector3f(north.x, north.y, north.z));
 }
 
 EarthCamera::~EarthCamera()
