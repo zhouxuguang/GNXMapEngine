015252914c6a0852d98218c11d3e894e5ef87c62
zhouxuguang <zhouxuguang236@126.com>
2025-06-15 19:00:10 +0800

    earth : 测试dxt1压缩，两种不同压缩方法的效率，ispc胜出，加上多线程，效率会更佳

--- GNXMapEngine/earthCore/TileLoadTask.cpp
+++ GNXMapEngine/earthCore/TileLoadTask.cpp
@@ -2,57 +2,42 @@
 #include "ImageCodec/ColorConverter.h"
 #include "RenderSystem/ImageTextureUtil.h"
 #include "AssetProcess/DXTCompressor.h"
+#include "BaseLib/LogService.h"
 #include "TiledImage.h"
 
 EARTH_CORE_NAMESPACE_BEGIN
 
 static Texture2DPtr TextureFromImage(const imagecodec::VImage& image)
 {
-	/*uint8_t* pDest = new uint8_t[image.GetImageSize()];
-	AssetProcess::CompressDXT1(pDest, image.GetPixels(), image.GetWidth(), image.GetHeight(), image.GetBytesPerRow());
-	FILE* fp = fopen("1.dds", "wb");
-	fwrite(pDest, 1, image.GetImageSize(), fp);
-	fclose(fp);
-	delete[] pDest;*/
+	VImagePtr dxt1Image = std::make_shared<VImage>();
+	dxt1Image->SetImageInfo(FORMAT_DXT1_RGB, image.GetWidth(), image.GetHeight());
+	dxt1Image->AllocPixels();
 
-	
+	baselib::TimeCost cost;
+	cost.Begin();
+	for (int i = 0; i < 1000; i ++)
+	{
+		AssetProcess::CompressDXT1(dxt1Image->GetPixels(), image.GetPixels(), image.GetWidth(), image.GetHeight(), image.GetBytesPerRow());
+	}
+	cost.End();
+	uint64_t t1 = cost.GetCostTimeNano();
+	LOG_INFO("CompressDXT1 cost %lf", (double)t1);
 
-	/*TextureDescriptor textureDescriptor = RenderSystem::ImageTextureUtil::getTextureDescriptor(image);
+	cost.Begin();
+	for (int i = 0; i < 1000; i++)
+	{
+		AssetProcess::CompressDXT1_ISPC(dxt1Image->GetPixels(), image.GetPixels(), image.GetWidth(), image.GetHeight(), image.GetBytesPerRow());
+	}
+	cost.End();
+	t1 = cost.GetCostTimeNano();
+	LOG_INFO("CompressDXT1_ISPC cost %lf", (double)t1);
+
+	TextureDescriptor textureDescriptor = RenderSystem::ImageTextureUtil::getTextureDescriptor(*dxt1Image);
 	textureDescriptor.mipmaped = true;
 
 	Texture2DPtr texture = getRenderDevice()->createTextureWithDescriptor(textureDescriptor);
 	Rect2D rect(0, 0, image.GetWidth(), image.GetHeight());
 	texture->replaceRegion(rect, dxt1Image->GetPixels());
-	return texture;*/
-
-	if (image.GetFormat() == imagecodec::FORMAT_SRGB8)
-	{
-		imagecodec::VImagePtr dstImage = std::make_shared<imagecodec::VImage>();
-		dstImage->SetImageInfo(imagecodec::FORMAT_RGBA8, image.GetWidth(), image.GetHeight());
-		dstImage->AllocPixels();
-		imagecodec::ColorConverter::convert_RGB24toRGBA32(image.GetPixels(), image.GetWidth() * image.GetHeight(), dstImage->GetPixels());
-
-		VImagePtr dxt1Image = std::make_shared<VImage>();
-		dxt1Image->SetImageInfo(FORMAT_DXT1_RGB, dstImage->GetWidth(), dstImage->GetHeight());
-		dxt1Image->AllocPixels();
-		AssetProcess::CompressDXT1(dxt1Image->GetPixels(), dstImage->GetPixels(), dstImage->GetWidth(), dstImage->GetHeight(), dstImage->GetBytesPerRow());
-		
-		TextureDescriptor textureDescriptor = RenderSystem::ImageTextureUtil::getTextureDescriptor(*dxt1Image);
-		textureDescriptor.mipmaped = true;
-
-		Texture2DPtr texture = getRenderDevice()->createTextureWithDescriptor(textureDescriptor);
-		Rect2D rect(0, 0, image.GetWidth(), image.GetHeight());
-		texture->replaceRegion(rect, dxt1Image->GetPixels());
-
-		return texture;
-	}
-
-	TextureDescriptor textureDescriptor = RenderSystem::ImageTextureUtil::getTextureDescriptor(image);
-	textureDescriptor.mipmaped = true;
-
-	Texture2DPtr texture = getRenderDevice()->createTextureWithDescriptor(textureDescriptor);
-	Rect2D rect(0, 0, image.GetWidth(), image.GetHeight());
-	texture->replaceRegion(rect, image.GetPixels());
 	return texture;
 }
 
